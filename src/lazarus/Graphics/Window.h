c3e5f798898a8a86a803989463b92cf956d01dad
monzo94 <dani.monzonis@gmail.com>
2019-10-03 21:17:48 +0200

    Document Window class

--- src/lazarus/Graphics/Window.h
+++ src/lazarus/Graphics/Window.h
@@ -8,25 +8,78 @@ namespace lz
 using sf::Color;
 using sf::Event;
 
+/**
+ * Handles the main window of the game.
+ * 
+ * The window is what will be shown to the user,
+ * and where all the sprites are rendered.
+ * 
+ * Coordinates in the window are given in tiles, where
+ * (0, 0) is the top-left corner, with the x-axis
+ * increasing to the right and the y-axis increasing
+ * when going down.
+ */
 class Window
 {
 public:
     // TODO: Layering/subwindow system
     Window(int width, int height, Color bg_color = Color::Black);
 
+    /**
+     * Returns the width in tiles of the window.
+     */
     int get_width() const;
+
+    /**
+     * Returns the height in tiles of the window.
+     */
     int get_height() const;
 
+    /**
+     * Loads a tileset and creates the window,
+     * using the tile size to calculate the final dimensions
+     * of the window.
+     */
     void load_tileset(const std::string &path, unsigned tile_size);
 
+    /**
+     * Draws the tile with the given ID from the tileset at the
+     * given position, in window coordinates.
+     * 
+     * If a tile was already drawn in the given position before
+     * flushing the window, it will be drawn on top.
+     * 
+     * @param pos Position in window coordinates where the tile should
+     * be drawn.
+     * @param tile_id ID of the tile in the tileset
+     * @param color Color multiplier to use for the tile
+     * to be drawn. By default, it is white, which preserves
+     * the original color.
+     */
     void set_tile(const Position2D &pos, int tile_id, Color color=Color::White);
 
+    /**
+     * Display the current contents of the window and
+     * flush the window buffer.
+     */
     void render();
 
+    /**
+     * Return whether the window is open.
+     */
     bool is_open() const;
 
+    /**
+     * Closes the window.
+     */
     void close();
 
+    /**
+     * Gets the next even in the queue.
+     * 
+     * @param event Reference in which the next event
+     * will be stored.
+     */
     bool poll_event(Event &event);
 
 private:
